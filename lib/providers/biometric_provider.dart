bc3ba7b3e9c3b1bea24e1cd917a6d018bb220223
AdrianoDiDio <95adriano@gmail.com>
2023-02-23 11:39:24 +0100

    Biometric:Moved enroll logic to provider

--- lib/providers/biometric_provider.dart
+++ lib/providers/biometric_provider.dart
@@ -1,8 +1,19 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:biometric_auth_frontend/biometrics/biometric_utils.dart';
+import 'package:biometric_auth_frontend/failures/error_object.dart';
+import 'package:biometric_auth_frontend/failures/failure.dart';
+import 'package:biometric_auth_frontend/generated/l10n.dart';
 import 'package:biometric_auth_frontend/locator.dart';
 import 'package:biometric_auth_frontend/logger.dart';
+import 'package:biometric_auth_frontend/repositories/biometric_auth_repository.dart';
+import 'package:biometric_auth_frontend/repositories/biometric_repository.dart';
+import 'package:biometric_auth_frontend/retrofit/responses/biometric_challenge_response.dart';
+import 'package:biometric_auth_frontend/retrofit/responses/biometric_token_response.dart';
 import 'package:biometric_auth_frontend/utils/storage_keys.dart';
 import 'package:biometric_auth_frontend/utils/storage_utils.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 
 class BiometricProvider with ChangeNotifier {
@@ -24,14 +35,80 @@ class BiometricProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  void enroll(String biometricToken, String privateKey, String userId) {
-    serviceLocator
-        .get<StorageUtils>()
-        .write(StorageKeys.biometricsToken, biometricToken);
-    serviceLocator
-        .get<StorageUtils>()
-        .write(StorageKeys.biometricsUserId, userId);
-    _biometricsEnrolled = true;
+  Future<String?> _getBiometricChallenge(
+      BiometricRepositoryImplementation biometricRepository) async {
+    return await biometricRepository
+        .getBiometricChallenge()
+        .then((value) => value.fold((l) => null, (r) => r.biometricChallenge));
+  }
+
+  Future<BiometricTokenResponse?> _getBiometricToken(
+      BiometricRepositoryImplementation biometricRepository,
+      String signedBiometricChallenge,
+      int nonce,
+      String publicKey) async {
+    return await biometricRepository
+        .getBiometricToken(signedBiometricChallenge, nonce, publicKey)
+        .then((value) => value.fold((l) => null, (r) => r));
+  }
+
+  Future<String?> _signPayload(
+      BiometricAuthRepositoryImplementation biometricAuthRepository,
+      String encodedChallengeWithNonce) async {
+    return await biometricAuthRepository
+        .signPayload(encodedChallengeWithNonce, S.current.biometricSignReason)
+        .then((value) => value.fold((l) => null, (r) => r));
+  }
+
+  Future<String?> _getPublicKey(
+      BiometricAuthRepositoryImplementation biometricAuthRepository) async {
+    return await biometricAuthRepository
+        .generateKeys()
+        .then((value) => value.fold((l) => null, (r) => r));
+  }
+
+  Future<void> enroll() async {
+    BiometricRepositoryImplementation biometricRepositoryImplementation =
+        BiometricRepositoryImplementation();
+    BiometricAuthRepositoryImplementation
+        biometricAuthRepositoryImplementation =
+        BiometricAuthRepositoryImplementation(
+            biometric: FlutterBiometricsImplementation());
+    _biometricsEnrolled = false;
+    String? publicKey =
+        await _getPublicKey(biometricAuthRepositoryImplementation);
+    if (publicKey != null) {
+      String? biometricChallenge =
+          await _getBiometricChallenge(biometricRepositoryImplementation);
+      if (biometricChallenge != null) {
+        String decodedChallenge = utf8.decode(base64Decode(biometricChallenge));
+        logger.d(
+            "Got challenge $biometricChallenge decoded to $decodedChallenge");
+        logger.d("Next step!");
+        Random random = Random();
+        var nonce = random.nextInt(4294967296);
+        String encodedChallengeWithNonce =
+            base64.encode(utf8.encode(decodedChallenge + nonce.toString()));
+        //Generate RSA key-pair
+        var signPayloadResponse = await _signPayload(
+            biometricAuthRepositoryImplementation, encodedChallengeWithNonce);
+        if (signPayloadResponse != null) {
+          var biometricTokenResponse = await _getBiometricToken(
+              biometricRepositoryImplementation,
+              signPayloadResponse,
+              nonce,
+              publicKey);
+          if (biometricTokenResponse != null) {
+            serviceLocator.get<StorageUtils>().write(
+                StorageKeys.biometricsToken,
+                biometricTokenResponse.biometricToken);
+            serviceLocator.get<StorageUtils>().write(
+                StorageKeys.biometricsUserId, biometricTokenResponse.userId);
+            _biometricsEnrolled = true;
+          }
+        }
+      }
+    }
     notifyListeners();
   }
 
--- lib/views/settings/biometric_settings_view.dart
+++ lib/views/settings/biometric_settings_view.dart
@@ -13,7 +13,6 @@ import 'package:biometric_auth_frontend/retrofit/responses/biometric_challenge_r
 import 'package:biometric_auth_frontend/size_config.dart';
 import 'package:dartz/dartz.dart' hide State;
 import 'package:flutter/material.dart';
-import 'package:flutter_biometrics/flutter_biometrics.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
@@ -58,70 +57,15 @@ class BiometricSettingsView extends StatelessWidget {
                 ]))));
   }
 
-  void _enrollBiometricFailed(BuildContext context) {
-    context.pop();
-  }
-
   void _enrollBiometricToken(BuildContext context) async {
-    BiometricRepositoryImplementation biometricRepositoryImplementation =
-        BiometricRepositoryImplementation();
     showDialog(
         barrierDismissible: false,
         context: context,
         builder: (context) {
           return const Center(child: CircularProgressIndicator(strokeWidth: 2));
         });
-    BiometricAuthRepositoryImplementation
-        biometricAuthRepositoryImplementation =
-        BiometricAuthRepositoryImplementation(
-            biometric: FlutterBiometricsImplementation());
-    var createKeyResponse =
-        await biometricAuthRepositoryImplementation.generateKeys();
-    createKeyResponse.fold((generateKeysFailure) {
-      ErrorObject errorObject =
-          ErrorObject.mapFailureToErrorObject(failure: generateKeysFailure);
-      logger.d("Couldn't generate a keypair...${errorObject.message}");
-      _enrollBiometricFailed(context);
-    }, (publicKey) async {
-      Either<Failure, BiometricTokenChallengeResponse>
-          biometricChallengeResponse =
-          await biometricRepositoryImplementation.getBiometricChallenge();
-      biometricChallengeResponse.fold((challengeFailure) {
-        ErrorObject errorObject =
-            ErrorObject.mapFailureToErrorObject(failure: challengeFailure);
-        logger.d("Couldn't generate a challenge...${errorObject.message}");
-        _enrollBiometricFailed(context);
-      }, (r) async {
-        String decodedChallenge =
-            utf8.decode(base64Decode(r.biometricChallenge));
-        logger.d(
-            "Got challenge ${r.biometricChallenge} decoded to $decodedChallenge");
-        logger.d("Next step!");
-        Random random = Random();
-        var nonce = random.nextInt(4294967296);
-        String encodedChallengeWithNonce =
-            base64.encode(utf8.encode(decodedChallenge + nonce.toString()));
-        //Generate RSA key-pair
-        var signPayloadResponse =
-            await biometricAuthRepositoryImplementation.signPayload(
-                encodedChallengeWithNonce, S.current.biometricSignReason);
-        signPayloadResponse.fold((signFailure) {
-          logger.d(
-              "Signing failed...${ErrorObject.mapFailureToErrorObject(failure: signFailure).message}");
-          _enrollBiometricFailed(context);
-        }, (signedChallenge) async {
-          var biometricTokenResponse = await biometricRepositoryImplementation
-              .getBiometricToken(signedChallenge, nonce, publicKey)
-              .whenComplete(() => context.pop());
-          biometricTokenResponse.fold((l) {
-            logger.d(
-                "Error:${ErrorObject.mapFailureToErrorObject(failure: l).message}");
-          }, (r) {
-            Provider.of<BiometricProvider>(context, listen: false)
-                .enroll(r.biometricToken, "biometric", r.userId);
-          });
-        });
-      });
-    });
+    await Provider.of<BiometricProvider>(context, listen: false)
+        .enroll()
+        .whenComplete(() => context.pop());
   }
 }
